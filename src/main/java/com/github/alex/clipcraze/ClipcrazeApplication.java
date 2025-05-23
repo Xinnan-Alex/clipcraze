815f21afd25f52e0a2db6110c4cb33e33824cf67
Xinnan-Alex <leongxinnan@outlook.my>
2025-05-23 12:11:05 +0800

    initial commit

---
+++ src/main/java/com/github/alex/clipcraze/ClipcrazeApplication.java
@@ -0,0 +1,13 @@
+package com.github.alex.clipcraze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipcrazeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipcrazeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/alex/clipcraze/ClipcrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alex.clipcraze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipcrazeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
